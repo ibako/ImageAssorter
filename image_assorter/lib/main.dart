00b5645f8fe67502b3faa7ea4635feea7419d12e
ibako <ibako.piyo@gmail.com>
2021-05-15 00:26:22 +0900

    ディレクトリ・ファイル選択のテスト実装

--- image_assorter/lib/main.dart
+++ image_assorter/lib/main.dart
@@ -1,113 +1,103 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_assorter/utils/file_dialog_util.dart' as fileDialogUtil;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Image Assorter',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      routes: {
+        '/': (context) => FilePickScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class FilePickScreen extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Card(
+          margin: EdgeInsets.all(16),
+          child: FilePickForm(),
+        )
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class FilePickForm extends StatefulWidget {
+  @override
+  _FilePickState createState() => _FilePickState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _FilePickState extends State<FilePickForm> {
+  final _rootPathTextController = TextEditingController();
+  final _selectedFileTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              flex: 4,
+              child: TextField(
+                controller: _rootPathTextController,
+                readOnly: true,
+                decoration: InputDecoration(hintText: 'Selected directory'),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              flex: 1,
+              child: ElevatedButton(
+                  child: Text('Select...'),
+                  onPressed: _selectDirectory
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Row(
+          children: [
+            Expanded(
+              flex: 4,
+              child: TextField(
+                controller: _selectedFileTextController,
+                readOnly: true,
+                decoration: InputDecoration(hintText: 'Selected image file'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: ElevatedButton(
+                child: Text('Select...'),
+                onPressed: _selectImage)
+            ),
+          ],
+        ),
+      ],
     );
+  }
+
+  void _selectDirectory() async {
+    final selectedDirectory = await fileDialogUtil.getDirectoryPath() ?? '';
+    setState(() {
+      _rootPathTextController.text = selectedDirectory;
+    });
+  }
+
+  void _selectImage() async {
+    final selectedImage = await fileDialogUtil.getImageFile();
+    final selectedPath = selectedImage?.path ?? '';
+    setState(() {
+      _selectedFileTextController.text = selectedPath;
+    });
   }
 }
---
+++ image_assorter/lib/utils/file_dialog_util.dart
@@ -0,0 +1,25 @@
+import 'package:file_selector_platform_interface/file_selector_platform_interface.dart';
+
+final XTypeGroup _imageXTypeGroup = XTypeGroup(
+  label: 'images',
+  extensions: ['jpg', 'jpeg', 'png', 'bmp', 'gif'],
+);
+
+Future<String?> getDirectoryPath() async {
+  return await FileSelectorPlatform.instance
+    .getDirectoryPath();
+}
+
+Future<XFile?> getImageFile() async {
+  return await FileSelectorPlatform.instance
+    .openFile(
+      acceptedTypeGroups: [_imageXTypeGroup],
+    );
+}
+
+Future<List<XFile>> getImageFiles() async {
+  return await FileSelectorPlatform.instance
+    .openFiles(
+      acceptedTypeGroups: [_imageXTypeGroup],
+    );
+}
